3bee2d09ca6e5f50efe5d2b1fca8b9405d8dffa0
Rudra2701 <rudra.shivhare.27@gmail.com>
2025-07-29 14:34:23 +0530

    Refactor neuron function and add Neuron class with weight and bias management

--- Neuron.py
+++ Neuron.py
@@ -1,3 +1,7 @@
+import random
+def makearr(x,l,u):
+    return [random.uniform(l,u) for i in range(x)]
+
 def multiply(a,b):
     c=[]
     for i in range(len(a)):
@@ -5,5 +9,24 @@ def multiply(a,b):
     return c
 
 def neuron(inputs,weights,bias=0):
-    return sum(multiply(inputs,weights))+bias
-print(neuron([1,2,3],[4,5,6],7))
+    return max(sum(multiply(inputs,weights))+bias,0)
+class Neuron:
+    def __init__(self,inputs,l=-1,u=1,bias=0):
+        #self.weights=weights
+        self.bias=bias
+        self.weights=makearr(inputs,l,u)
+    def get(self,inputs):
+        return neuron(inputs,self.weights,self.bias)
+    def weights(self,x,weight):
+        self.weights[x]=weight
+    def bias(self,bias):
+        self.bias=bias
+    def get_weights(self):
+        return self.weights
+    def get_bias(self):
+        return self.bias
+    def change_weights(self,weights):
+        for i in range(len(weights)):
+            self.weights+=i
+    def change_bias(self,bias):
+        self.bias+=bias
