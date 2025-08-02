ea532411fa55745e3ad06596031866158037fd7c
Rudra2701 <rudra.shivhare.27@gmail.com>
2025-08-02 13:31:00 +0530

    Add Network class and improve Layer initialization with bounds
    - Introduced `Network` class for managing layers, weights, and biases.
    - Enhanced `Layer` class to allow configurable weight and bias initialization bounds.
    - Updated `Neuron` class with minor code formatting adjustments.
    - Renamed LICENSE to LICENSE.md.

--- Layer.py
+++ Layer.py
@@ -3,13 +3,14 @@ from Neuron import makearr
 
 
 class Layer:
-    def __init__(self,inputs,outputs):
+    def __init__(self,inputs,outputs,lower=-1,upper=1):
         self.outputs=outputs
-        x=makearr(inputs,-1,1)
+        x=makearr(inputs,lower,upper)
         self.weights=[]
+
         for i in range(outputs):
             self.weights.append(x)
-        self.bias=makearr(outputs,0,0)
+        self.bias=makearr(outputs,lower,upper)
     def feedforward(self,inputs):
         o=[]
         for i in range(self.outputs):
@@ -29,18 +30,3 @@ class Layer:
     def change_bias(self,bias):
         for i in range(len(bias)):
             self.bias[i]+=bias[i]
-"""
-   Copyright 2025 Rudra Sahil Shivhare
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
--- Neuron.py
+++ Neuron.py
@@ -14,7 +14,7 @@ class Neuron:
     def __init__(self,inputs,l=-1,u=1,bias=0):
         #self.weights=weights
         self.bias=bias
-        self.weights=makearr(inputs,l,u)
+        self.weights = makearr(inputs, l, u)
     def get(self,inputs):
         return neuron(inputs,self.weights,self.bias)
     def weights(self,x,weight):
@@ -30,18 +30,3 @@ class Neuron:
             self.weights+=i
     def change_bias(self,bias):
         self.bias+=bias
-"""
-   Copyright 2025 Rudra Sahil Shivhare
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-"""
